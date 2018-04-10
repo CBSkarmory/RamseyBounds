c8a9d0645256c1053d0ed02e0d677d10e4cb8544
CBSkarmory <CBSkarmory@users.noreply.github.com>
2018-04-10 12:33:41 -0400

    graph clique finding test code

--- src/main/java/Bounds3Col.java
+++ src/main/java/Bounds3Col.java
@@ -1,3 +1,13 @@
+import org.jgrapht.EdgeFactory;
+import org.jgrapht.Graph;
+import org.jgrapht.alg.clique.BronKerboschCliqueFinder;
+import org.jgrapht.graph.ClassBasedEdgeFactory;
+import org.jgrapht.graph.DefaultEdge;
+import org.jgrapht.graph.SimpleGraph;
+
+import java.util.Iterator;
+import java.util.Set;
+
 /**
  * This will obtain bounds on R(a, b, c) for 1 <= a <= b <= c <= 10
  * R(a, b, c) is least n such that for all 3-colorings of an n-clique,
@@ -10,6 +20,22 @@
 public class Bounds3Col {
     public static void main(String[] args) {
         // testing
-        System.out.println("hello world");
+        EdgeFactory<Integer, DefaultEdge> edgeFactory = new ClassBasedEdgeFactory<Integer, DefaultEdge>(DefaultEdge.class);
+        Graph<Integer, DefaultEdge> g = new SimpleGraph<Integer, DefaultEdge>(edgeFactory);
+
+        int N = 2;
+        //create clique of size N
+        for (int i = 1; i <= N; ++i) {
+            g.addVertex(i);
+            for (Integer v : g.vertexSet()) {
+                if (i != v) {
+                    g.addEdge(i, v);
+                }
+            }
+        }
+        BronKerboschCliqueFinder<Integer, DefaultEdge> finder = new BronKerboschCliqueFinder<Integer, DefaultEdge>(g);
+        Iterator<Set<Integer>> it = finder.maximumIterator();
+        Set<Integer> clique1 = it.next();
+        System.out.println(clique1);
     }
 }
