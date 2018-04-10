aa72dfeb22296cc22dd82b53901c67c7d028542d
CBSkarmory <CBSkarmory@users.noreply.github.com>
2018-04-10 11:28:47 -0400

    building jar and testing code

---
+++ src/main/java/Bounds3Col.java
@@ -0,0 +1,15 @@
+/**
+ * This will obtain bounds on R(a, b, c) for 1 <= a <= b <= c <= 10
+ * R(a, b, c) is least n such that for all 3-colorings of an n-clique,
+ * there exists either
+ * (1) RED a-clique
+ * (2) BLUE b-clique
+ * (3) GREEN c-clique
+ */
+
+public class Bounds3Col {
+    public static void main(String[] args) {
+        // testing
+        System.out.println("hello world");
+    }
+}
