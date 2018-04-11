1585a4ac275acb493e1e11eede7dd87e404d6095
CBSkarmory <CBSkarmory@users.noreply.github.com>
2018-04-11 13:19:47 -0400

    changed bound to 10

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -28,12 +28,13 @@ public class Runner {
         Iterator<Set<Integer>> it = finder.maximumIterator();
         Set<Integer> clique1 = it.next();
         System.out.println(clique1);*/
+
         // bounds on numbers
         Ramsey3ColBounder bounder = new Ramsey3ColBounder();
-        int N = 4;
-        for (int a = 2; a < N; ++a) {
-            for (int b = a; b < N; ++b) {
-                for (int c = b; c < N; ++c) {
+        int N = 10;
+        for (int a = 2; a <= N; ++a) {
+            for (int b = a; b <= N; ++b) {
+                for (int c = b; c <= N; ++c) {
                     System.out.printf("R(%d, %d, %d) <= %s\n", a, b, c, bounder.boundR(a, b, c).toString());
                 }
             }
